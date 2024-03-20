a14fa8adba47269cd5dc3fd585a4c90b0936a2da
Andertk0 <129784797+Andertk0@users.noreply.github.com>
2024-03-20 09:36:59 -0300

    Contornos suaves + Refinamento do layout

--- sections/Content/Hero.tsx
+++ sections/Content/Hero.tsx
@@ -20,8 +20,8 @@ export interface Props {
 }
 
 const PLACEMENT = {
-  left: "flex-col text-left lg:flex-row-reverse",
-  right: "flex-col text-left lg:flex-row",
+  left: "flex-col text-left md:flex-row-reverse",
+  right: "flex-col text-left md:flex-row",
 };
 
 export default function HeroFlats({
@@ -35,16 +35,16 @@ export default function HeroFlats({
     <div>
       <div class="mx-auto flex flex-col items-center gap-8">
         <div
-          class={`flex w-full xl:container xl:mx-auto py-20 mx-5 md:mx-10 z-10 ${
+          class={`flex lg:w-full xl:container xl:mx-auto py-20 mx-5 md:mx-10 z-10 ${
             image
               ? PLACEMENT[placement]
               : "flex-col items-center justify-center text-center"
-          } lg:py-36 gap-12 md:gap-20 items-center`}
+          } lg:py-36 gap-5 sm:p-3 items-center`}
         >
           {image && (
             <Image
               width={640}
-              class="w-full lg:w-1/2 object-fit"
+              class="w-full lg:w-1/2 object-fit rounded-lg"
               sizes="(max-width: 640px) 100vw, 30vw"
               src={image}
               alt={image}
@@ -61,15 +61,13 @@ export default function HeroFlats({
           >
             <div
               class="inline-block text-[80px] leading-[100%] font-medium tracking-[-2.4px]"
-              dangerouslySetInnerHTML={{
-                __html: title,
-              }}
+              dangerouslySetInnerHTML={{ __html: title }}
             >
             </div>
             <p class="text-zinc-400 text-[16px] md:text-[18px] leading-[150%]">
               {description}
             </p>
-            <div class="flex flex-col items-center lg:items-start lg:flex-row gap-4">
+            <div class="flex flex-col items-center md:items-start lg:flex-row gap-4">
               {cta?.map((item) => (
                 <a
                   key={item?.id}

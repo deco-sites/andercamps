96f8fb4d758b6d8ae20214716a731989fecd88d9
Anderson Santos da Silva Filho <129784797+Andertk0@users.noreply.github.com>
2024-04-07 22:50:44 +0000

    Partial Gallery (Initial)

---
+++ sections/Conexões Culturais/PartialImageGallery.tsx
@@ -0,0 +1,28 @@
+import { ImageWidget } from "apps/admin/widgets.ts";
+import PartialGalleryButton from "../../islands/GalleryButton.tsx";
+
+export interface Props {
+  /**
+   * @minItems 3
+   */
+  images?: ImageWidget[];
+}
+
+export default function PartialImageGallery({ images }: Props) {
+  if (!images || !images.length) images = [];
+  images = images.filter((i) => typeof i === "string");
+
+  // Verificar se o número de imagens é menor que 3
+  if (images && images.length < 3) {
+    const placeholders = 3 - images.length;
+    for (let i = 0; i < placeholders; i++) {
+      images.push(`https://fakeimg.pl/300x300`);
+    }
+  }
+
+  return (
+    <>
+      <PartialGalleryButton images={images} />
+    </>
+  );
+}
